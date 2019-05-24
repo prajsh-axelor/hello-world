685f1be2756a3716612e54778ab24262802a2bc6
EncryptpedMe <38177972+EncryptpedMe@users.noreply.github.com>
2019-05-24 10:10:55 +0530

    Added a new SOP and did some change in existing one.

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 class helloworld{
 	public static void main(String [] args){
-		System.out.print("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("Testing Pull request");
 	}
 }
